1f514afdd52b49f1284a5c815f334d5109fdfbb6
CarlosJaramilloTinajero <75448585+CarlosJaramilloTinajero@users.noreply.github.com>
2023-07-17 19:19:21 -0600

    Terget Blank para el link de agregar ver peliculas con Vue

--- resources/views/api-omba/add.blade.php
+++ resources/views/api-omba/add.blade.php
@@ -7,7 +7,7 @@
     <div class="container p-4" style="margin-top: 100px;">
         <h3 class="text-center">Agregar una nueva pelicula</h3>
         <div class="d-flex justify-content-center mb-5">
-            <a href="{{ route('vue.movies') }}" class="btn btn-link">Usar API con Vue 3</a>
+            <a href="{{ route('vue.movies') }}" class="btn btn-link" target="_blank">Usar API con Vue 3</a>
         </div>
         @livewire('movies.add-movie-omba')
         {{-- @livewire('movies.add-movie-modal') --}}
